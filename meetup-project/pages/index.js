f498245760b3eaef299535cb0825e2bd17d71a17
iqranaz240 <iqranaz240@gmail.com>
2023-05-13 23:35:45 +0500

    Sec23/Nextjs done till getStaticProps and SSR

--- meetup-project/pages/index.js
+++ meetup-project/pages/index.js
@@ -19,8 +19,31 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  }; 
 }
 
 export default HomePage;
