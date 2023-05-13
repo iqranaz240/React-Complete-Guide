c2935b572d146fea65ace9d303cbf2c64df9635d
iqranaz240 <iqranaz240@gmail.com>
2023-05-13 23:26:37 +0500

    empty repo

--- complete-project/frontend/src/pages/EditEvent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EditEvent() {
-  return (
-    <div>
-      <h1>Edit Events</h1>
-    </div>
-  )
-}
--- complete-project/frontend/src/pages/NewEvent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NewEvent() {
-  return (
-    <div>
-      <h1>New Event</h1>
-    </div>
-  )
-}
